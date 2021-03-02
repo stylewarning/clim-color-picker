7aac0b297fa1854b2dd7c189a1d6efebc85be055
Robert Smith <robert@stylewarning.com>
2021-03-02 10:18:52 -0800

    add daniel's fixes from issue #1

--- gui.lisp
+++ gui.lisp
@@ -6,47 +6,47 @@
 (define-application-frame color-picker ()
   ()
   (:menu-bar nil)
-  (:panes 
+  (:panes
    (display :application
-	    :scroll-bars nil
-	    :display-function 'display-color)
-   (exit push-button 
-	 :label "Exit"
-	 :activate-callback (lambda (button)
-			      (frame-exit (pane-frame button))))
+            :scroll-bars nil
+            :display-function 'display-color)
+   (exit push-button
+         :label "Exit"
+         :activate-callback (lambda (button)
+                              (frame-exit (pane-frame button))))
    (red slider
-	:label "Red" :foreground +red+
-	:orientation :horizontal
-        :value 0.0
-	:min-value 0.0 :max-value 1.0 
-	:show-value-p t :decimal-places 3
-	:client 'color :id 'red)
+        :label "Red" :foreground +red+
+        :orientation :horizontal
+        :value 0.2
+        :min-value 0.0 :max-value 1.0
+        :show-value-p t :decimal-places 3
+        :client 'color :id 'red)
    (green slider
-	  :label "Green" :foreground +green+
-	  :orientation :horizontal
-          :value 0.0
-	  :min-value 0.0 :max-value 1.0 
-	  :show-value-p t :decimal-places 3
-	  :client 'color :id 'green)
+          :label "Green" :foreground +green+
+          :orientation :horizontal
+          :value 0.4
+          :min-value 0.0 :max-value 1.0
+          :show-value-p t :decimal-places 3
+          :client 'color :id 'green)
    (blue slider
-	 :label "Blue" :foreground +blue+
-	 :orientation :horizontal
-         :value 0.0
-	 :min-value 0.0 :max-value 1.0 
-	 :show-value-p t :decimal-places 3
-	 :client 'color :id 'blue))
+         :label "Blue" :foreground +blue+
+         :orientation :horizontal
+         :value 0.6
+         :min-value 0.0 :max-value 1.0
+         :show-value-p t :decimal-places 3
+         :client 'color :id 'blue))
   (:layouts
-   (default 
+   (default
     (vertically ()
-      (outlining ()
-	(horizontally ()
-          display
-          exit))
-      (outlining ()
-	(vertically ()
-          red
-          green
-          blue))))))
+      (1/3
+       (horizontally ()
+         (1/2 display)
+         (1/2 exit)))
+      (2/3
+       (vertically ()
+         red
+         green
+         blue))))))
 
 (defun frame-color (frame)
   (make-rgb-color (gadget-value (find-pane-named frame 'red))
@@ -54,9 +54,8 @@
                   (gadget-value (find-pane-named frame 'blue))))
 
 (defun display-color (frame pane)
-  (with-bounding-rectangle* (left top right bottom) (window-viewport pane)
-    (with-output-recording-options (pane :record nil)
-      (draw-rectangle* pane left top right bottom :ink (frame-color frame)))))
+  (with-bounding-rectangle* (left top right bottom) pane
+    (draw-rectangle* pane left top right bottom :ink (frame-color frame))))
 
 (defmethod value-changed-callback :after ((slider slider) (client (eql 'color)) id value)
   (declare (ignore id value))
